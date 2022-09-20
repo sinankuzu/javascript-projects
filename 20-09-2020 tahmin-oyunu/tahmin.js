030b1545828706e8967a1f6f65986b81a02d0569
Sinan <sinankzu@gmail.com>
2022-09-20 20:01:20 +0200

    ittir

--- 20-09-2020 tahmin-oyunu/tahmin.js
+++ 20-09-2020 tahmin-oyunu/tahmin.js
@@ -6,41 +6,48 @@ const winLose = document.querySelector(".win-lose")
 const yazilanSayi = document.querySelector(".yazilan-sayi");
 const kalanHak = document.querySelector(".kalan-hak")
 const bildinMi = document.querySelector(".bildin-mi")
+const gifNedir = document.querySelector(".gifler2")
 let score = 10;
 console.log(sayi);
 
+// gifNedir.onmouseover = () => {
+//   gifNedir.src = "./img/fail-hay.gif";
+// };
 
 checkButton.onclick = () => {
     
      const girilenTahmin = document.querySelector(".tahmin").value;
-     yazilanSayi.innerHTML = `girilen sayi: ${girilenTahmin}`;
+     yazilanSayi.innerHTML = `The Number You Have Entered Is: ${girilenTahmin}`;
      
     if (score > 1 && girilenTahmin != sayi) {
     
-      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector("body").style.backgroundColor = "crimson";
       score -= 1;
       console.log(score);
-      kalanHak.innerHTML = `Kalan hakkiniz: ${score}`;
+      kalanHak.innerHTML = `Remaining Tries: ${score}`;
       if (Number(girilenTahmin) > sayi) {
-        arttirAzalt.innerHTML = "Azalt";
+        arttirAzalt.innerHTML = "LESS!!!";
       } else {
-        arttirAzalt.innerHTML = "Arttir";
+        arttirAzalt.innerHTML = "MORE!!!";
       }
     }
     else if(girilenTahmin == sayi) {
       winLose.innerHTML = "YOU WON! NICE!!!";
       document.querySelector("body").style.backgroundColor = "green";
-      yazilanSayi.innerHTML = "girilen sayi:?";
-      kalanHak.innerHTML = `Kalan hakkiniz:?`;
-      arttirAzalt.innerHTML ="Girdiginiz sayiya gore burda arttir veya azalt yazicak";
+      yazilanSayi.innerHTML = "The Number You Have Entered Is:?";
+      kalanHak.innerHTML = `Remaining Tries:?`;
+      arttirAzalt.innerHTML = "MORE or LESS";
       bildinMi.innerHTML = `${sayi}`;
+      gifNedir.src = "./img/success.gif";
     }
   
   else{
+    gifNedir.src = "./img/fail-hay.gif";
     winLose.innerHTML = "GAME OVER YOU LOST";
-    kalanHak.innerHTML = `Kalan hakkiniz:0`;
-    yazilanSayi.innerHTML = "girilen sayi:?";
-    arttirAzalt.innerHTML ="Girdiginiz sayiya gore burda arttir veya azalt yazicak";
+    kalanHak.innerHTML = `Remaining Tries:0`;
+    yazilanSayi.innerHTML = "The Number You Have Entered Is:?";
+    arttirAzalt.innerHTML = "MORE or LESS";
+    document.querySelector("body").style.backgroundColor = "red";
   }
 }
 
@@ -50,10 +57,11 @@ againButton.onclick=()=>{
     document.querySelector("body").style.backgroundColor = "grey";
     score = 10;
     sayi = Math.floor(Math.random() * 20);
-    kalanHak.innerHTML = `Kalan hakkiniz:10`;
-    yazilanSayi.innerHTML = "girilen sayi:?";
+    kalanHak.innerHTML = `Remaining Tries:10`;
+    yazilanSayi.innerHTML = "The Number You Have Entered Is:?";
     winLose.innerHTML = "";
-    arttirAzalt.innerHTML = "Girdiginiz sayiya gore burda arttir veya azalt yazicak";
+    arttirAzalt.innerHTML = "MORE or LESS";
     bildinMi.innerHTML = `?`;
     console.log(sayi);
+    gifNedir.src = "./img/calculating.gif";
 }
