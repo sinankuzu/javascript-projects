89e59cabcb37bff16ca99636f094b4d81ae0067c
Sinan <sinankzu@gmail.com>
2022-09-23 18:02:36 +0200

    aa

--- ios calculator/app.js
+++ ios calculator/app.js
@@ -1,34 +1,54 @@
 const numberButtons = document.querySelectorAll(".numbers")
-// const myNumber = document.querySelector()
 const screen1 = document.querySelector(".screen1")
 const screenOperator = document.querySelector(".screenOperator")
 const screen2 = document.querySelector(".screen2")
 const calculator = document.querySelector(".calculator")
 const operators = document.querySelector(".orange")
+let sonuc = "";
 
- console.log(numberButtons.forEach((x)=>console.log(x)));
-
- 
-numberButtons[0].onclick = () =>{
-screen1.textContent = numberButtons[0].textContent;
-console.log(screen1.textContent);
-}
 
 calculator.onclick = (e) =>{
-    if(e.target.classList.contains("numbers")){
-        screen2.textContent = e.target.textContent;
-    }
+if(screen2.textContent == ""){
+let y = "";
+let x = "";
 
+}
+    if(e.target.classList.contains("numbers")){
+        screen2.textContent += e.target.textContent;
+        
+        x = Number(screen2.textContent)
+        y = Number(screen1.textContent)
+    }
+    
+    
     else if(e.target.classList.contains("orange")){
+        if(y == ""){
         screenOperator.textContent = e.target.textContent;
         screen1.textContent = screen2.textContent;
-        screen2.textContent = "";  
-        if(e.target.textContent == "+"){
-          screenOperator.textContent = e.target.textContent;
-          screen1.textContent = screen2.textContent;
-          screen2.textContent = "";  
+        screen2.textContent = "";
         }
-        
+
+        else if (e.target.textContent == "=" || true) {
+          if (e.target.textContent == "+") {
+            screen2.textContent = y + x;
+            sonuc = y + x;
+          } else if (e.target.textContent == "-") {
+            screen2.textContent = y - x;
+            sonuc = y - x;
+          } else if (e.target.textContent == "x") {
+            screen2.textContent = y * x;
+            sonuc = y * x;
+          } else if (e.target.textContent == "÷") {
+            screen2.textContent = y / x;
+            sonuc = y / x;
+          } 
+          
+          screen1.textContent = screen2.textContent;
+          screen2.textContent = "";
+        }
+          
         
     }
 }
+
+
