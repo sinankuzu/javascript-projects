74c94058ff36872cfd4087d7994d329372b6991a
Sinan <sinankzu@gmail.com>
2022-09-26 22:40:16 +0200

    aa

--- ios calculator/app.js
+++ ios calculator/app.js
@@ -6,45 +6,59 @@ const calculator = document.querySelector(".calculator");
 const operators = document.querySelector(".orange");
 let sonuc = document.querySelector(".result");
 const sifir = document.querySelector(".number0");
-const myDot = document.querySelector(".dot")
+const myDot = document.querySelector(".dot");
 // // const myDel = document.querySelector(".ac")
 // const plusMinus = document.querySelector(".plus-minus")
 // let sonuc = "";
 let islem = "";
-let a = ""
+let a = "";
 
 calculator.onclick = (e) => {
   const hesapMakina = function (z) {
-    screen2.textContent = sonuc.textContent;
-    sonuc = "";
-    
-    if (e.target.textContent == "+") {
+    // screen2.textContent = sonuc.textContent;
+    // sonuc = "";
+
+    if (islem == "+") {
       screen2.textContent = y + x;
       sonuc = y + x;
-      islem = "+";
-    } else if (e.target.textContent == "-") {
+    } else if (islem == "-") {
       screen2.textContent = y - x;
       sonuc = y - x;
-      islem = "-";
-    } else if (e.target.textContent == "x") {
+    } else if (islem == "x") {
       screen2.textContent = y * x;
       sonuc = y * x;
-      islem = "*";
-    } else if (e.target.textContent == "÷") {
+    } else if (islem == "÷") {
       screen2.textContent = y / x;
       sonuc = y / x;
-      islem = "/";
     }
+    // screen2.textContent = sonuc;
+    // if (e.target.textContent == "+") {
+    //   screen2.textContent = y + x;
+    //   sonuc = y + x;
+    //   islem = "+";
+    // } else if (e.target.textContent == "-") {
+    //   screen2.textContent = y - x;
+    //   sonuc = y - x;
+    //   islem = "-";
+    // } else if (e.target.textContent == "x") {
+    //   screen2.textContent = y * x;
+    //   sonuc = y * x;
+    //   islem = "*";
+    // } else if (e.target.textContent == "÷") {
+    //   screen2.textContent = y / x;
+    //   sonuc = y / x;
+    //   islem = "/";
+    // }
 
     islem = screenOperator.textContent;
     screen1.textContent = screen2.textContent;
     screen2.textContent = "";
-    
+
     if (e.target.textContent == "=") {
       if (islem == "+") {
         screen2.textContent = y + x;
         sonuc = y + x;
-        
+
       } else if (islem == "-") {
         screen2.textContent = y - x;
         sonuc=y - x;
@@ -52,21 +66,17 @@ calculator.onclick = (e) => {
         screen2.textContent = y * x;
         sonuc = y * x;
       } else if (islem == "÷") {
-        // screen2.textContent = y / x;
+        screen2.textContent = y / x;
         sonuc = y / x;
       }
-      
+
       screen1.textContent = "";
       screenOperator.textContent = "";
       screen2.textContent = sonuc;
     }
-    
   };
- 
+
   if (e.target.classList.contains("numbers")) {
-   
-   
-    
     if (
       screen2.textContent.startsWith("0") &&
       e.target.textContent == "0" &&
@@ -86,40 +96,31 @@ calculator.onclick = (e) => {
       y = Number(screen1.textContent);
       x = Number(screen2.textContent);
     }
-  if (screen2.textContent.startsWith(".")) {
-    screen2.textContent = "0.";
-  }   
-  if (screen2.textContent.includes(".")) {
-    myDot.classList.add("block");
-  } 
-  
-    
-  
-  } 
-  else if (e.target.classList.contains("orange")) {
-    
+    if (screen2.textContent.startsWith(".")) {
+      screen2.textContent = "0.";
+    }
+    if (screen2.textContent.includes(".")) {
+      myDot.classList.add("block");
+    }
+  }
+  if (e.target.classList.contains("orange")) {
+    screenOperator.textContent = e.target.textContent;
     if (y == "") {
-      screenOperator.textContent = e.target.textContent;
       screen1.textContent = screen2.textContent;
       screen2.textContent = "";
     } else {
       hesapMakina(e.target.textContent);
-      screenOperator.textContent = e.target.textContent;
     }
-    
-   
   }
-if(e.target.classList.contains("perc")){
-  if(screen1.textContent ==""){
-  screen2.textContent = screen2.textContent * 0.01;
+  if (e.target.classList.contains("perc")) {
+    if (screen1.textContent == "") {
+      screen2.textContent = screen2.textContent * 0.01;
+    } else {
+      screen2.textContent = screen1.textContent * (screen2.textContent * 0.01);
+      screen1.textContent = "";
+      screenOperator.textContent = "";
+    }
   }
-  else{
-    screen2.textContent = screen1.textContent * (screen2.textContent * 0.01);
-    screen1.textContent = "";
-    screenOperator.textContent = "";
-    
-  }
-}
 
   if (e.target.classList.contains("ac")) {
     screen1.textContent = "";
@@ -127,10 +128,8 @@ if(e.target.classList.contains("perc")){
     screen2.textContent = "";
   }
 
-  if(e.target.classList.contains("plus-minus")){
-    screen2.textContent = screen2.textContent * -1
-    
-  
+  if (e.target.classList.contains("plus-minus")) {
+    screen2.textContent = screen2.textContent * -1;
   }
   if (screen2.textContent == "") {
     myDot.classList.remove("block");
