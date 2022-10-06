ff0b261195ce824b293215cba19b30d549c63072
Sinan <sinankzu@gmail.com>
2022-10-06 02:11:34 +0200

    yedek

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const myApiKey = "d9b53b6af071e91297e071e6ffc3e133";
-const myCards = document.querySelector(".container");
+const myCards = document.querySelector(".cards");
 const input = document.querySelector("input");
 const button = document.querySelector("button")
 const getData = (city) =>
@@ -18,9 +18,9 @@ const printScreen = (city) => {
 const iconUrl = `http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`;
 myCards.innerHTML += `<div class="card">
     <div class="names">${city.name}</div>
-    <div class="celcius">${city.main.temp}</div>
+    <div class="celcius">${Math.ceil(city.main.temp)}<sup>°</sup></div>
     <div class="icons"><img src="${iconUrl}"></img></div>
-    <div class="condition">${city.weather[0].description}</div>`
+    <div class="condition">${city.weather[0].description}</div>`;
 }
 
 
